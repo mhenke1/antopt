2b501c04fc1ca251ad458924a9579bccc81fac03
mhenke <mhenke@IBM-5612U0WKZBX.boeblingen.de.ibm.com>
2012-04-05 16:43:52 +0200

    initialize leg data finished

--- src/antopt/core.clj
+++ src/antopt/core.clj
@@ -1,6 +1,14 @@
 (ns antopt.core)
   (:require clojure.math.combinatorics)
 
+(def random (java.util.Random.)) 
+  
+(def alpha 1)
+(def beta 2)
+(def rho 0.5)
+(def number-of-ants 500)
+(def number-of-generations 50) 
+  
 (def cities [
 	[37, 52], [49, 49], [52, 64], [20, 26], [40, 30], [21, 47],
 	[17, 63], [31, 62], [52, 33], [51, 21], [42, 41], [31, 32],
@@ -13,13 +21,17 @@
 	[48, 28], [56, 37], [30, 40]
 ])
 
-(def alpha 0.5)
-(def beta 2)
+(defn create-leg-info [leg cities]
+	(let [[city1 city2] leg
+		  leg-dist (distance (cities city1) (cities city2))
+		  weighted-distance (Math/pow leg-dist beta) 
+		  tau (* (rand) 0.1)
+		  weighted-tau (Math/pow tau alpha)]
+		  (list leg-dist weighted-distance tau weighted-tau)))
 
-(defn initialize-data [cities] 
-	(let [leg-data {} 
-		  all-legs (cartesian-product (range (count cities)) (range (count cities)))]
-		 (reduce merge (map (fn [leg] (assoc leg-data leg 1)) all-legs))))
+(defn initialize-leg-data [cities] 
+	(let [all-legs (cartesian-product (range (count cities)) (range (count cities)))]
+		 (reduce merge (map (fn [leg] {leg (create-leg-info leg cities)}) all-legs))))
 
 (defn distance [point1 point2] 
 	(let [[x1 y1] point1 [x2 y2] point2] 
