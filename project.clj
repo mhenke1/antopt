7e6604af1b1eb5d3d568d7f09c1aba62b4a10a6e
Martin Henke <martin.henke@web.de>
2012-05-08 21:39:30 +0200

    change dependency to clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject antopt "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"] 
+  :dependencies [[org.clojure/clojure "1.4.0"] 
   [org.clojure/math.combinatorics "0.0.2"]])
