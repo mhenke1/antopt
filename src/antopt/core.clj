e72d352b1bdd48f9138060de008f0763e976876c
Martin Henke <martin.henke@web.de>
2012-11-19 16:09:35 +0100

    Rewrite First working version Tests incomplete

--- src/antopt/core.clj
+++ src/antopt/core.clj
@@ -24,122 +24,117 @@
 	[point1 point2] 
 	(let [[x1 y1] point1 [x2 y2] point2] 
 		(Math/sqrt (+ (Math/pow (- x2 x1) 2) (Math/pow (- y2 y1) 2)))))
-	
-(defn leg-distance 
+
+(defn connection-length 
 	"Calculates euclidian distance between two cities"
-	[leg cities] 
-	(let [[city1 city2] leg]
-		(euclidian-distance (cities city1) (cities city2))))
+	[connection cities] 
+	(let [[city-id1 city-id2] connection]
+		(euclidian-distance (cities city-id1) (cities city-id2))))
     
 (defn tour-length 
 	"Calculates the total length of a given tour"
 	[tour cities] 
-    (let [legs-in-tour (partition 2  1 tour)
-    	 tour-length  (reduce + (map #(leg-distance % cities) legs-in-tour))]
+    (let [connections-in-tour (partition 2  1 tour)
+    	 tour-length  (reduce + (map #(connection-length  % cities) connections-in-tour))]
     	 (if (>= (count tour) 2) 
-    	   		(+ tour-length (leg-distance [(first tour) (peek tour)] cities))
+    	   		(+ tour-length (connection-length  [(first tour) (peek tour)] cities))
     	    	tour-length)))	
 
-(defn initialize-leg-info 
-	"Inititialize all data of a connection between two cities"
-	[leg cities]
-	(let [[city1 city2] leg
-		  distance (leg-distance leg cities)
+(defn create-connection-data 
+	"Inititialize all data for a connection between two cities"
+	[connection cities]
+	(let [[city1 city2] connection
+		  distance (connection-length connection cities)
 		  weighted-distance (Math/pow distance beta) 
 		  tau (* (rand) 0.1)
 		  weighted-tau (Math/pow tau alpha)
 		  probability (/ weighted-tau weighted-distance)]
 		  {:distance distance :weighted-distance weighted-distance :tau tau :weighted-tau weighted-tau :probability probability}))
-    
-(defn initialize-leg-data 
+
+(defn initialize-all-connections 
 	"Inititialize the data of all connections between the given cities"
 	[cities] 
-	(let [all-legs (filter (fn [[x y]] (not= x y)) (cartesian-product (range (count cities)) (range (count cities))))]
-		(reduce merge (map (fn [leg] {(vec leg) (initialize-leg-info leg cities)}) all-legs))))
-    
-(defn evaporate-leg 
+	(let [all-connections (filter (fn [[x y]] (not= x y)) (cartesian-product (range (count cities)) (range (count cities))))]
+		(reduce merge (map (fn [connection] {(vec connection) (create-connection-data connection cities)}) all-connections))))
+
+(defn evaporate-connection-data 
 	"Evaporates pheromone on a connection between two cities"
-	[leg-id leg-info] 
-	(let [{:keys [distance weighted-distance tau]} leg-info
+	[connection-id connection-data] 
+	(let [{:keys [distance weighted-distance tau]} connection-data
 		new-tau (* tau (- 1 rho))
 		new-weighted-tau (Math/pow new-tau alpha)
 		new-probability (/ new-weighted-tau weighted-distance)]
-		{leg-id {:distance distance :weighted-distance weighted-distance :tau new-tau :weighted-tau new-weighted-tau :probability new-probability}}))
+		{connection-id {:distance distance :weighted-distance weighted-distance :tau new-tau :weighted-tau new-weighted-tau :probability new-probability}}))
 
-(defn evaporate-pheromone 
+(defn evaporate-all-connections
 	"Evaporates pheromone on all connections between two cities"
-	[leg-data]
-  	(reduce merge (map (fn [leg] (evaporate-leg (first leg) (last leg))) leg-data)))
-
+	[connection-data]
+  	(reduce merge (map (fn [connection] (evaporate-connection-data (first connection) (last connection))) connection-data)))
 
 (defn adjust-pheromone-for-tour
 	"Amplifies pehoromone a tour walked by an ant"
-	[leg-data tour-with-length]
+	[connection-data tour-with-length]
 	(let [[tour-length tour] tour-with-length
-		legs-in-tour (vec (map vec (partition 2  1 tour)))]
-		(loop [leg-data leg-data legs-in-tour legs-in-tour]
-			(if (empty? legs-in-tour)
-				leg-data
-				(let [leg-id (first legs-in-tour)
-					leg-info (leg-data leg-id)
-				 	{:keys [distance weighted-distance tau]} leg-info
+		connections-in-tour (vec (map vec (partition 2  1 tour)))]
+		(loop [connection-data connection-data connections-in-tour connections-in-tour]
+			(if (empty? connections-in-tour)
+				connection-data
+				(let [connection-id (first connections-in-tour)
+					connection-info (connection-data connection-id)
+				 	{:keys [distance weighted-distance tau]} connection-info
 					new-tau (+ tau (/ 1 tour-length))
 					new-weighted-tau (Math/pow new-tau alpha)
 					new-probability (/ new-weighted-tau weighted-distance)
-					new-leg-data (assoc leg-data leg-id {:distance distance :weighted-distance weighted-distance :tau new-tau :weighted-tau new-weighted-tau :probability new-probability})]
-					(recur new-leg-data (rest legs-in-tour)))))))
+					new-connection-data (assoc connection-data connection-id {:distance distance :weighted-distance weighted-distance :tau new-tau :weighted-tau new-weighted-tau :probability new-probability})]
+					(recur new-connection-data (rest connections-in-tour)))))))
 
-(defn adjust-pheromone-for-tours
+(defn adjust-pheromone-for-multiple-tours
 	"Amplifies pehoromone a tour walked by a generation of ants"
-	[leg-data tours-with-length]
+	[connection-data tours-with-length]
 	(if (not (empty? tours-with-length))
-		(let [new-leg-data (adjust-pheromone-for-tour leg-data (first tours-with-length))]
-			(recur new-leg-data (vec (rest tours-with-length))))
-			leg-data)) 
+		(let [new-connection-data (adjust-pheromone-for-tour connection-data (first tours-with-length))]
+			(recur new-connection-data (vec (rest tours-with-length))))
+			connection-data)) 
 
 (defn choose-next-city 
-	[leg-data current-city remaining-cities]
+	[connection-data current-city remaining-cities]
 	(let [current-city-list (vec (repeat (count remaining-cities) current-city))
 		connections (vec (map vector current-city-list remaining-cities))
-		added-probabilities (reduce + (map (fn [connection] (:probability (leg-data connection))) connections))
+		added-probabilities (reduce + (map (fn [connection] (:probability (connection-data connection))) connections))
 		limit (* (rand) added-probabilities)]
 		(loop [probabilities 0 next-city current-city remaining-connections connections]
 			(if (and (< probabilities limit) (not (empty? remaining-connections)))				
-				(let [new-probabilities (+ probabilities (:probability (leg-data (first remaining-connections))))]
+				(let [new-probabilities (+ probabilities (:probability (connection-data (first remaining-connections))))]
 					(recur new-probabilities (last (first remaining-connections)) (rest remaining-connections)))
 				next-city))))
 
-(defn ant-walk-tour
-	[leg-data cities]
+(defn walk-ant-tour
+	[connection-data cities]
 	(let [cities-list (range 1 (count cities))]
 		(loop [tour [0] remaining-cities cities-list]
 			(if (or (empty? tour) (empty? remaining-cities))
 				[(tour-length tour cities) tour]
-				(let [next-city (choose-next-city leg-data (peek tour) remaining-cities)
+				(let [next-city (choose-next-city connection-data (peek tour) remaining-cities)
 					new-tour (conj tour next-city)
 					new-remaining-cities (remove #(= % next-city) remaining-cities)]
 					(recur new-tour new-remaining-cities))))))
-	
+
 (defn one-generation-ant-tours
-	[leg-data ant-number cities]
-	(let [tour-list (map (fn [ant] (ant-walk-tour leg-data cities)) (range ant-number))
-		shortest-tour (apply min-key first tour-list)
-		new-leg-data (-> leg-data (adjust-pheromone-for-tour (vec (last shortest-tour))) (adjust-pheromone-for-tour (vec (reverse (last shortest-tour))) (evaporate-pheromone)))]
-		[new-leg-data tour-list shortest-tour]))
+	[connection-data ant-number cities]
+	(let [tour-list (map (fn [ant] (walk-ant-tour connection-data cities)) (range ant-number))
+		shortest-tour (apply min-key  first tour-list)
+		reversed-shortest-tour [(first shortest-tour) (vec (reverse (last shortest-tour)))]
+		new-connection-data (-> connection-data (adjust-pheromone-for-tour shortest-tour) (adjust-pheromone-for-tour reversed-shortest-tour) (evaporate-all-connections))]
+		{:new-connection-data new-connection-data :generation-shortest-tour shortest-tour}))
 
-(defn one-generation-ant-tours2
-	[leg-data ant-number cities]
-	(let [tour-list (map (fn [ant] (ant-walk-tour leg-data cities)) (range ant-number))
-		shortest-tour (apply min-key first tour-list)
-		new-leg-data (last shortest-tour)]
-		(println tour-list ":" shortest-tour ":" new-leg-data)))
-
-; (defn multi-generation-ant-tour
-; 	[generations ant-number cities]
-	; (let [leg-data (initialize-leg-data cities)]
-; 		(loop [leg-data leg-data generations generations shortest-tour [10000000 []]]
-; 			(let [[new-leg-data  tour-list new-shortest-tour] (one-generation-ant-tours leg-data ant-number cities)]
-; 				(println min-tour)
-; 				(if (= generations 0) 
-; 					new-leg-data
-; 					(recur new-leg-data (- generations 1) new-shortest-tor))))))
+(defn antopt
+	[]
+	(let [connection-data (initialize-all-connections cities-on-map)]
+		(loop [number-of-generations number-of-generations connection-data connection-data shortest-tour [Long/MAX_VALUE []]]
+			(if (> number-of-generations 0) 
+				(let [{:keys [new-connection-data generation-shortest-tour]} (one-generation-ant-tours connection-data number-of-ants cities-on-map)]
+					(if (>= (first generation-shortest-tour) (first shortest-tour))
+						(recur (- number-of-generations 1) new-connection-data shortest-tour)
+						(do 
+							(println number-of-generations ":" generation-shortest-tour)
+							(recur (- number-of-generations 1) new-connection-data generation-shortest-tour))))))))
--- test/antopt/test/core.clj
+++ test/antopt/test/core.clj
@@ -12,109 +12,113 @@
 	(is (= 0.0 (euclidian-distance [0 0] [0 0])))
 	(is (= 5.0 (euclidian-distance [0 0] [4 3]))))
 
-(deftest test-leg-distance
-	(is (= 0.0 (leg-distance [0 1] [[0 0] [0 0]])))
-	(is (= 5.0 (leg-distance [0 1] [[0 0] [4 3]]))))
+(deftest test-connection-length 
+	(is (= 0.0 (connection-length  [0 1] [[0 0] [0 0]])))
+	(is (= 5.0 (connection-length  [0 1] [[0 0] [4 3]]))))
 
 (deftest test-tour-length
 	(is (= 0 (tour-length [0] [0 0])))
 	(is (= 10.0 (tour-length [0 1] [[0 0] [4 3]])))
 	(is (= 10.0 (tour-length [0 2 1] [[0 0] [0 0] [4 3]]))))
 
-(deftest test-initialize-leg-info 
-	(let [test-info (initialize-leg-info [0 1] [[0 0] [4 3]])]
+(deftest test-create-connection-data 
+	(let [test-info (create-connection-data [0 1] [[0 0] [4 3]])]
 		(is (= 5.0 (:distance test-info)))
 		(is (= 25.0 (:weighted-distance test-info)))
 		(is (> 0.1 (:tau test-info))))) 
 
-(deftest test-initialize-leg-data 
-	(let [test-info1 (test-data [0 1])
-		test-info2 (test-data [1 0])
-		test-info3 (test-data [0 0])
-		test-info4 (test-data [1 1])]
-		(is (= 5.0 (:distance test-info1)))
-		(is (= 5.0 (:distance test-info2)))
-		(is (= nil (:distance test-info3)))
-		(is (= nil (:distance test-info4)))
-		(is (= (:distance test-info1) (:distance test-info2)))))
+(deftest test-initialize-all-connections 
+	(let [connections (initialize-all-connections [[0 0] [4 3]])
+		test-info1 (connections [0 1])
+        test-info2 (connections [1 0])]
+        (is (= 5.0 (:distance test-info1)))
+        (is (= 5.0 (:distance test-info2)))
+        (is (= 25.0 (:weighted-distance test-info1)))
+        (is (= 25.0 (:weighted-distance test-info2)))
+        (is (= (:distance test-info1) (:distance test-info2)))
+        (is (= (:weighted-distance test-info1) (:weighted-distance test-info2)))))
 
-(deftest test-evaporate-leg
-	(let [test-leg (test-data [0 1])
-		 test-evap-data (evaporate-leg [0 1] test-leg)
-		 test-evap (test-evap-data [0 1])]
-		(is (= (:distance test-evap) (:distance test-leg)))
-		(is (< (:tau test-evap) (:tau test-leg)))
-		(is (< (:weighted-tau test-evap) (:weighted-tau test-leg)))
-		(is (= (:weighted-distance test-evap) (:weighted-distance test-leg)))
-		(is (< (:probability test-evap) (:probability test-leg)))))
+(deftest test-evaporate-connection-data 
+    (let [test-connection (test-data [0 1])
+         test-evap-data (evaporate-connection-data [0 1] test-connection)
+         test-evap (test-evap-data [0 1])]
+        (is (= (:distance test-evap) (:distance test-connection)))
+        (is (< (:tau test-evap) (:tau test-connection)))
+        (is (< (:weighted-tau test-evap) (:weighted-tau test-connection)))
+        (is (= (:weighted-distance test-evap) (:weighted-distance test-connection)))
+        (is (< (:probability test-evap) (:probability test-connection)))))
 
 (deftest test-evaporate-pheromone
-	(let [test-evap-data (evaporate-pheromone test-data)
-		test-leg1 (test-data [0 1])
-		test-evap1 (test-evap-data [0 1])
-		test-leg2 (test-data [1 0])
-		test-evap2 (test-evap-data [1 0])]
-		(is (= (:distance test-evap1) (:distance test-leg1)))
-		(is (< (:tau test-evap1) (:tau test-leg1)))
-		(is (< (:weighted-tau test-evap1) (:weighted-tau test-leg1)))
-		(is (= (:weighted-distance test-evap1) (:weighted-distance test-leg1)))
-		(is (< (:probability test-evap1) (:probability test-leg1)))
-		(is (= (:distance test-evap2) (:distance test-leg2)))
-		(is (< (:tau test-evap2) (:tau test-leg2)))
-		(is (< (:weighted-tau test-evap2) (:weighted-tau test-leg2)))
-		(is (= (:weighted-distance test-evap2) (:weighted-distance test-leg2)))
-		(is (< (:probability test-evap2) (:probability test-leg2)))))
+    (let [test-evap-data (evaporate-all-connections test-data)
+	    test-connection1 (test-data [0 1])
+	    test-evap1 (test-evap-data [0 1])
+	    test-connection2 (test-data [1 0])
+	    test-evap2 (test-evap-data [1 0])]
+	    (is (= (:distance test-evap1) (:distance test-connection1)))
+	    (is (< (:tau test-evap1) (:tau test-connection1)))
+	    (is (< (:weighted-tau test-evap1) (:weighted-tau test-connection1)))
+	    (is (= (:weighted-distance test-evap1) (:weighted-distance test-connection1)))
+	    (is (< (:probability test-evap1) (:probability test-connection1)))
+	    (is (= (:distance test-evap2) (:distance test-connection2)))
+	    (is (< (:tau test-evap2) (:tau test-connection2)))
+	    (is (< (:weighted-tau test-evap2) (:weighted-tau test-connection2)))
+	    (is (= (:weighted-distance test-evap2) (:weighted-distance test-connection2)))
+	    (is (< (:probability test-evap2) (:probability test-connection2)))))
 
 (deftest test-adjust-pheromone-for-tour
-	(let [evap-data (adjust-pheromone-for-tour test-data [10 [0 1 2]])
-		test-leg1 (test-data [0 1])
-		test-leg2 (test-data [1 2])
-		test-evap1 (evap-data [0 1])
-		test-evap2 (evap-data [1 2])]
-		(is (= (:distance test-evap1) (:distance test-leg1)))
-		(is (> (:tau test-evap1) (:tau test-leg1)))
-		(is (> (:weighted-tau test-evap1) (:weighted-tau test-leg1)))
-		(is (= (:weighted-distance test-evap1) (:weighted-distance test-leg1)))
-		(is (> (:probability test-evap1) (:probability test-leg1)))
-		(is (= (:distance test-evap2) (:distance test-leg2)))
-		(is (> (:tau test-evap2) (:tau test-leg2)))
-		(is (> (:weighted-tau test-evap2) (:weighted-tau test-leg2)))
-		(is (= (:weighted-distance test-evap2) (:weighted-distance test-leg2)))
-		(is (> (:probability test-evap2) (:probability test-leg2)))))
+    (let [evap-data (adjust-pheromone-for-tour test-data [10 [0 1 2]])
+        test-connection1 (test-data [0 1])
+        test-connection2 (test-data [1 2])
+        test-evap1 (evap-data [0 1])
+        test-evap2 (evap-data [1 2])]
+        (is (= (:distance test-evap1) (:distance test-connection1)))
+        (is (> (:tau test-evap1) (:tau test-connection1)))
+        (is (> (:weighted-tau test-evap1) (:weighted-tau test-connection1)))
+        (is (= (:weighted-distance test-evap1) (:weighted-distance test-connection1)))
+        (is (> (:probability test-evap1) (:probability test-connection1)))
+        (is (= (:distance test-evap2) (:distance test-connection2)))
+        (is (> (:tau test-evap2) (:tau test-connection2)))
+        (is (> (:weighted-tau test-evap2) (:weighted-tau test-connection2)))
+        (is (= (:weighted-distance test-evap2) (:weighted-distance test-connection2)))
+        (is (> (:probability test-evap2) (:probability test-connection2)))))
 
-(deftest test-adjust-pheromone-for-tours
-	(let [evap-data (adjust-pheromone-for-tours test-data [[10 [0 1 2]] [5 [2 1 0]]])
-		test-leg1 (test-data [0 1])
-		test-leg2 (test-data [1 2])
-		test-evap1 (evap-data [0 1])
-		test-evap2 (evap-data [1 2])]
-		(is (= (:distance test-evap1) (:distance test-leg1)))
-		(is (> (:tau test-evap1) (:tau test-leg1)))
-		(is (> (:weighted-tau test-evap1) (:weighted-tau test-leg1)))
-		(is (= (:weighted-distance test-evap1) (:weighted-distance test-leg1)))
-		(is (> (:probability test-evap1) (:probability test-leg1)))
-		(is (= (:distance test-evap2) (:distance test-leg2)))
-		(is (> (:tau test-evap2) (:tau test-leg2)))
-		(is (> (:weighted-tau test-evap2) (:weighted-tau test-leg2)))
-		(is (= (:weighted-distance test-evap2) (:weighted-distance test-leg2)))
-		(is (> (:probability test-evap2) (:probability test-leg2)))))
+(deftest test-adjust-pheromone-for-multiple-tours
+    (let [evap-data (adjust-pheromone-for-multiple-tours test-data [[10 [0 1 2]] [5 [2 1 0]]])
+        test-connection1 (test-data [0 1])
+        test-connection2 (test-data [1 2])
+        test-evap1 (evap-data [0 1])
+        test-evap2 (evap-data [1 2])]
+        (is (= (:distance test-evap1) (:distance test-connection1)))
+        (is (> (:tau test-evap1) (:tau test-connection1)))
+        (is (> (:weighted-tau test-evap1) (:weighted-tau test-connection1)))
+        (is (= (:weighted-distance test-evap1) (:weighted-distance test-connection1)))
+        (is (> (:probability test-evap1) (:probability test-connection1)))
+        (is (= (:distance test-evap2) (:distance test-connection2)))
+        (is (> (:tau test-evap2) (:tau test-connection2)))
+        (is (> (:weighted-tau test-evap2) (:weighted-tau test-connection2)))
+        (is (= (:weighted-distance test-evap2) (:weighted-distance test-connection2)))
+        (is (> (:probability test-evap2) (:probability test-connection2)))))
 
 (deftest test-choose-next-city
-	(let [next-city1 (choose-next-city test-data 0 [1 2])
-		next-city2 (choose-next-city test-data 0 [])]
-		(is (some #{next-city1} [1 2]))
-		(is (= next-city2 0))))
+    (let [next-city1 (choose-next-city test-data 0 [1 2])
+    	next-city2 (choose-next-city test-data 0 [])]
+        (is (some #{next-city1} [1 2]))
+        (is (= next-city2 0))))
 
-(deftest test-ant-walk-tour
-	(let [[ant-tour-length tour] (ant-walk-tour (initialize-leg-data cities-on-map) cities-on-map)]
-	;	(println ant-tour-length ":" tour)
-		(is (= (count tour) (count cities-on-map)))
-		(is (= (count tour) (count (set tour))))
-		(is (some #{0} tour))
-		(is (some #{1} tour))
-		(is (some #{2} tour))
-		(is (= 0 (first tour)))))
+(deftest test-walk-ant-tour
+    (let [[ant-tour-length tour] (walk-ant-tour (initialize-all-connections cities-on-map) cities-on-map)]
+;       (println ant-tour-length ":" tour)
+        (is (= (count tour) (count cities-on-map)))
+        (is (= (count tour) (count (set tour))))
+        (is (some #{0} tour))
+        (is (some #{1} tour))
+        (is (some #{2} tour))
+        (is (= 0 (first tour)))))
 
 (deftest test-one-generation-ant-tours 
-	(let [foo (one-generation-ant-tours2 (initialize-leg-data cities-on-map) 5 cities-on-map)]
-	(is (= 1 1))))
+        (let [foo (one-generation-ant-tours (initialize-all-connections cities-on-map) 5 cities-on-map)]
+        (is (= 1 1))))
+
+(deftest test-antopt 
+        (let [foo (antopt)]
+        (is (= 1 1))))
