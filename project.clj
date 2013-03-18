912e939fef72d90953999f4c2d2994fb80c78552
Martin Henke <martin.henke@web.de>
2013-03-18 15:59:56 +0100

    update dependency to tools.reader 0.7.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject antopt "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.1"] [seesaw "1.4.3"] [org.clojure/tools.reader "0.7.2"]]
+  :dependencies [[org.clojure/clojure "1.5.1"] [seesaw "1.4.3"] [org.clojure/tools.reader "0.7.3"]]
   :main antoptui.core)
