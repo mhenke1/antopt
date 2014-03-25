a9fe68865d6f0bc7c018ab79cbe63601f7f8f991
Martin Henke <martin.henke@web.de>
2014-03-25 22:39:10 +0100

    update to clojure 1.6.0 and seesaw 1.4.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject antopt "1.0.0-SNAPSHOT"
   :description "Traveling salesman using ant colony optimization"
-  :dependencies [[org.clojure/clojure "1.5.1"] [seesaw "1.4.3"]]
+  :dependencies [[org.clojure/clojure "1.6.0"] [seesaw "1.4.4"]]
   :main antopt.antoptui)
