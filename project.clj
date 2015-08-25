9773192ac9be5ab0998d929a469eb08e8d539239
Martin Henke <martin.henke@web.de>
2015-08-26 00:23:02 +0200

    update to clojure 1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject antopt "1.0.0-SNAPSHOT"
   :description "Traveling salesman using ant colony optimization"
-  :dependencies [[org.clojure/clojure "1.6.0"] [seesaw "1.4.5"]]
+  :dependencies [[org.clojure/clojure "1.7.0"] [seesaw "1.4.5"]]
   :main antopt.antoptui)
